2f519acec436cc0c74b61eb1eff583229cd30cff
Myles <kumyles@gmail.com>
2024-05-27 17:37:12 -0400

    Fix integer regex

--- src/pages/cryptography/ecc/InputsForm.vue
+++ src/pages/cryptography/ecc/InputsForm.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import InputLabel from "@/components/InputLabel.vue";
 import TextInput from "@/components/TextInput.vue";
-import { numeric } from "@/utils/filters";
+import { allIntegers } from "@/utils/filters";
 import { ref } from "vue";
 
 const text = ref("");
@@ -10,12 +10,21 @@ const text = ref("");
 <template>
   <div class="flex-col items-center flex grow content-center w-full">
     <h3 class="font-bold underline text-3xl pb-2">Inputs</h3>
-    <form class="flex flex-col gap-2">
-      <InputLabel for="a">
-        <template #text>a</template>
-        <template #tool-tip>The equation of the elliptic curve used</template>
-      </InputLabel>
-      <TextInput id="a" v-model="text" :filter="numeric" />
+    <form class="flex flex-col gap-2 text-xl">
+      <div>
+        <InputLabel for="a" class="italic" inputmode="numeric">
+          <template #text>a</template>
+          <template #tool-tip>The equation of the elliptic curve used</template>
+        </InputLabel>
+        <TextInput id="a" v-model="text" :filter="allIntegers" />
+      </div>
+      <div>
+        <InputLabel for="b" class="italic" inputmode="numeric">
+          <template #text>b</template>
+          <template #tool-tip>The equation of the elliptic curve used</template>
+        </InputLabel>
+        <TextInput id="b" v-model="text" :filter="allIntegers" />
+      </div>
     </form>
   </div>
 </template>
--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,3 +1,7 @@
-export function numeric(input: string) {
-  return input.replace(/^0+|[^\d.]/g, "");
+export function allIntegers(input: string) {
+  if (input[0] === "-") {
+    return "-" + input.substring(1).replace(/^0+|[^\d.]/g, "");
+  } else {
+    return input.replace(/^0+|[^\d.]/g, "");
+  }
 }
