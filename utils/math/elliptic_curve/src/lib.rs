ede2beb20ceec5c9dc4bd6306fca27209d8ac591
Myles <kumyles@gmail.com>
2024-05-29 16:06:18 -0400

    Elliptic curve additio ntesting

--- utils/math/elliptic_curve/src/lib.rs
+++ utils/math/elliptic_curve/src/lib.rs
@@ -21,11 +21,13 @@ impl Curve {
     }
 
     fn add (self, p: &Point, q: &Point) -> Point {
-        let slope: i128 = if p != q {
-            (q.y - p.y) / (q.x- p.x)
+        let slope = if p != q {
+            (q.y - p.y) / (q.x - p.x)
         } else {
             (3 * ( p.x * p.x) + self.a ) / (2 * p.y)
         };
+
+
 
         let x = slope * slope - p.x - q.x;
         let y = slope * (p.x - x) - p.y;
@@ -42,10 +44,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn add() {
+    fn add_p_not_equal_q() {
         let p = Point::new(1, 2);
         let q = Point::new(3, 4);
-        let curve = Curve::new(1, 1);
+        let curve = Curve::new(-7, 10);
         assert_eq!(Point::new(-3 ,2), curve.add(&p, &q));
+    }
+
+    #[test]
+    fn add_p_equals_q() {
+        let p = Point::new(1, 2);
+        let q = Point::new(1, 2);
+        let curve = Curve::new(-7, 10);
+        assert_eq!(Point::new(-1, -4), curve.add(&p, &q));
     }
 }
