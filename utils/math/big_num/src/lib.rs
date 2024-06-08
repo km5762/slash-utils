1455a3b7390702eef841c72b7b6b1f7043e3b656
Myles <kumyles@gmail.com>
2024-06-08 17:02:43 -0400

    Before in place shifting

--- utils/math/big_num/src/lib.rs
+++ utils/math/big_num/src/lib.rs
@@ -6,7 +6,7 @@ const RADIX: u64 = u32::MAX as u64 + 1;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 struct BigUInt<const N: usize> {
-    limbs: [u32; N]
+    limbs: [u32; N],
 }
 
 impl<const N: usize> Default for BigUInt<N> {
@@ -18,6 +18,18 @@ impl<const N: usize> Default for BigUInt<N> {
 }
 
 impl <const N: usize> BigUInt<N> {
+    fn new(limbs: [u32; N]) -> Self{
+        BigUInt {limbs}
+    }
+
+    fn from_u64(n: u64) -> Self {
+        let mut limbs = [0; N];
+        limbs[0] = n as u32;
+        limbs[1] = (n >> 32) as u32;
+
+        BigUInt::new(limbs)
+    }
+
     fn from_str_radix(src: &str, radix: u32) -> Self {
         let mut num: BigUInt<N> = BigUInt::default();
 
@@ -55,7 +67,7 @@ impl <const N: usize> BigUInt<N> {
             k = cur % n64;
         }
 
-        (BigUInt {limbs: w}, k as u32)
+        (BigUInt::new(w), k as u32)
     }
 
     fn mul_u32(&self, n: u32) -> Self {
@@ -73,7 +85,7 @@ impl <const N: usize> BigUInt<N> {
             panic!("integer overflow");
         }
 
-        BigUInt {limbs: w}
+        BigUInt::new(w)
     }
 
     fn add_u32(&self, n: u32) -> Self {
@@ -95,7 +107,132 @@ impl <const N: usize> BigUInt<N> {
             }
         }
 
-        BigUInt { limbs: w }
+        BigUInt::new(w)
+    }
+
+    fn shl_carry(&self, n: u32) -> (Self, u32) {
+        if n == 0 {return (BigUInt::new(self.limbs), 0)}
+        let mut limbs = [0; N];
+        let mut carry = 0;
+
+        for i in 0..N {
+            limbs[i] = self.limbs[i] << n | carry;
+            carry = self.limbs[i] >> (32 - n);
+        }
+
+        (BigUInt::new(limbs), carry)
+    }
+
+    fn shr_carry(self, n: u32) -> (Self, u32) {
+        if n == 0 {return (BigUInt::new(self.limbs), 0)}
+        let mut limbs = [0; N];
+        let mut carry = 0;
+
+        for i in (0..N).rev() {
+            limbs[i] = self.limbs[i] >> n | carry;
+            carry = self.limbs[i] & ((1 >> n) - 1);
+        }
+
+        (BigUInt::new(limbs), carry)
+    }
+
+    fn leading_zeros(&self) -> u32 {
+        let mut zeros = 0;
+
+        for i in (0..N).rev() {
+            if (self.limbs[i] > 0) {
+                break;
+            } else {
+                zeros += 1;
+            }
+        }
+
+        zeros
+    }
+
+    fn checked_mul(&self, rhs: Self) -> Option<Self> {
+        let mut w = [0; N];
+        let mut k = 0;
+        
+        for j in 0..N {
+            k = 0;
+            if rhs.limbs[j] == 0 {continue};
+            for i in 0..N {
+                if i + j >= N {
+                    if self.limbs[i] != 0 || rhs.limbs[i] != 0 {
+                        return None;
+                    }
+                } else {
+                    let t = u64::from(self.limbs[i]) * u64::from(rhs.limbs[j]) + u64::from(w[i + j]) + k;
+                    w[i + j] = (t % RADIX) as u32;
+                    k = t / RADIX;
+                }
+            }
+        }
+
+        if k > 0 {
+            return None;
+        }
+
+        Some(BigUInt::new(w))
+    }
+
+    fn div_rem(&self, rhs: Self) -> (Self, Self) {
+        let dividend_digits = N - self.leading_zeros() as usize;
+        let divisor_digits = N - rhs.leading_zeros() as usize;
+
+        let shift = rhs.limbs[divisor_digits - 1].leading_zeros();
+        let (mut dividend,  dividend_overflow) = self.shl_carry(shift);
+        let (divisor,  _) = rhs.shl_carry(shift);
+        let mut q: BigUInt<N> = BigUInt::default();
+
+        for i in (0..=(dividend_digits - divisor_digits)).rev() {
+            let msd = if i == (dividend_digits - divisor_digits) {
+                dividend_overflow
+            } else {
+                dividend.limbs[divisor_digits + i]
+            };
+            let smsd = dividend.limbs[divisor_digits + i - 1];
+
+            let dividend_msd_combined = u64::from(msd) * RADIX + u64::from(smsd);
+            let mut q_estimate = dividend_msd_combined / u64::from(divisor.limbs[divisor_digits - 1]);
+            let mut r_estimate = dividend_msd_combined % u64::from(divisor.limbs[divisor_digits - 1]);
+            
+        
+            loop {
+                if q_estimate >= RADIX 
+                || ((q_estimate * u64::from(divisor.limbs[divisor_digits - 2])) > (r_estimate * RADIX + u64::from(dividend.limbs[divisor_digits - 2 + i])))
+                {
+                    q_estimate -= 1;
+                    r_estimate += u64::from(divisor.limbs[divisor_digits - 1]);
+                    if r_estimate < RADIX {continue};
+                }
+                break;
+            }
+            
+            // Based on previous loop, q_estimate must fit within 32 bits now
+            let mut carry: i64 = 0;
+            for j in 0..divisor_digits {
+                let subtrahend = q_estimate * u64::from(divisor.limbs[i]);
+                let difference = i64::from(dividend.limbs[i + j]) - carry - ((subtrahend & 0xFFFFFFFF) as i64);
+                dividend.limbs[i + j] = difference as u32;
+                carry = i64::from((subtrahend >> 32) as u32) - i64::from((difference >> 32) as u32);
+            }
+            let final_digit = i64::from(divisor.limbs[i + divisor_digits]) - carry;
+            
+            q.limbs[i] = q_estimate as u32;
+            if final_digit < 0 {
+                let mut carry = 0;
+                for j in 0..divisor_digits {
+                    let sum = u64::from(divisor.limbs[i + j]) + u64::from(divisor.limbs[i]) + carry;
+                    dividend.limbs[i + j] = sum as u32;
+                    carry = sum >> 32;
+                }
+                dividend.limbs[i + divisor_digits] = dividend.limbs[i + divisor_digits] + carry as u32;
+            }
+        }
+        
+        (q, dividend >> shift)
     }
 }
 
@@ -120,7 +257,15 @@ impl <const N: usize> core::ops::Add for BigUInt<N> {
             }
         }
 
-        BigUInt { limbs: w }
+        BigUInt::new(w)
+    }
+}
+
+impl <const N: usize> core::ops::Shr<u32> for BigUInt<N> {
+    type Output = Self;
+
+    fn shr(self, rhs: u32) -> Self::Output {
+        self.shr_carry(rhs).0
     }
 }
 
@@ -144,7 +289,36 @@ impl <const N: usize> core::ops::Sub for BigUInt<N> {
             }
         }
 
-        BigUInt { limbs: w }
+        BigUInt::new(w)
+    }
+}
+
+impl <const N: usize> core::cmp::PartialOrd for BigUInt<N> {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        for i in (0..N).rev() {
+            if self.limbs[i] > other.limbs[i] {
+                return Some(core::cmp::Ordering::Greater);
+            } else if self.limbs[i] < other.limbs[i] {
+                return Some(core::cmp::Ordering::Less);
+            }
+        }
+        Some(core::cmp::Ordering::Equal)
+    }
+}
+
+impl <const N: usize> core::ops::Div for BigUInt<N> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        self.div_rem(rhs).0
+    }
+}
+
+impl <const N: usize> core::ops::Rem for BigUInt<N> {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        self.div_rem(rhs).1
     }
 }
 
@@ -152,30 +326,7 @@ impl <const N: usize> core::ops::Mul for BigUInt<N> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        let mut w = [0; N];
-        let mut k = 0;
-        
-        for j in 0..N {
-            k = 0;
-            if rhs.limbs[j] == 0 {continue};
-            for i in 0..N {
-                if i + j >= N {
-                    if self.limbs[i] != 0 || rhs.limbs[i] != 0 {
-                        panic!("integer overflow");
-                    }
-                } else {
-                    let t = u64::from(self.limbs[i]) * u64::from(rhs.limbs[j]) + u64::from(w[i + j]) + k;
-                    w[i + j] = (t % RADIX) as u32;
-                    k = t / RADIX;
-                }
-            }
-        }
-
-        if k > 0 {
-            panic!("integer overflow");
-        }
-
-        BigUInt { limbs: w }
+        self.checked_mul(rhs).expect("integer overflow")
     }
 }
 
@@ -185,32 +336,32 @@ mod tests {
 
     #[test]
     fn add() {
-        let a = BigUInt { limbs: [100, 200, 300] };
-        let b = BigUInt { limbs: [400, 500, 600] };
+        let a = BigUInt::new([100, 200, 300]);
+        let b = BigUInt::new([400, 500, 600]);
         let result = a + b;
         assert_eq!(result.limbs, [500, 700, 900]);
     }
 
     #[test]
     fn add_with_carry() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, 0] };
-        let b = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0]);
+        let b = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0]);
         let result = a + b;
         assert_eq!(result.limbs, [u32::MAX - 1, u32::MAX , u32::MAX, 1]); // Check for carry propagation
     }
 
     #[test]
     fn add_with_0() {
-        let a = BigUInt { limbs: [100, 200, 300] };
-        let b = BigUInt { limbs: [0, 0, 0] };
+        let a = BigUInt::new([100, 200, 300]);
+        let b = BigUInt::new([0, 0, 0]);
         let result = a + b;
         assert_eq!(result.limbs, [100, 200, 300]); // Adding zero should not change the value
     }
 
     #[test]
     fn add_with_carry_propagation() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, 0] };
-        let b = BigUInt { limbs: [1, 0, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0]);
+        let b = BigUInt::new([1, 0, 0, 0]);
         let result = a + b;
         assert_eq!(result.limbs, [0, 0, 0, 1]);
     }
@@ -218,47 +369,47 @@ mod tests {
     #[test]
     #[should_panic]
     fn add_panic_on_overflow() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX] };
-        let b = BigUInt { limbs: [1, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX]);
+        let b = BigUInt::new([1, 0, 0]);
         let _ = a + b;
     }
 
     #[test]
     fn mul_with_0() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, u32::MAX] };
-        let b = BigUInt { limbs: [0, 0, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, u32::MAX]);
+        let b = BigUInt::new([0, 0, 0, 0]);
         let result = a * b;
         assert_eq!(result.limbs, [0, 0, 0, 0]);
     }
 
     #[test]
     fn mul_with_1() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, u32::MAX] };
-        let b = BigUInt { limbs: [1, 0, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, u32::MAX]);
+        let b = BigUInt::new([1, 0, 0, 0]);
         let result = a * b;
         assert_eq!(result.limbs, [u32::MAX, u32::MAX, u32::MAX, u32::MAX]);
     }
 
     #[test]
     fn mul_double() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, 0] };
-        let b = BigUInt { limbs: [2, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, 0]);
+        let b = BigUInt::new([2, 0, 0]);
         let result = a * b;
         assert_eq!(result.limbs, [u32::MAX - 1, u32::MAX, 1]);
     }
 
     #[test]
     fn mul_single_digit() {
-        let a = BigUInt { limbs: [12345] };
-        let b = BigUInt { limbs: [6789] };
+        let a = BigUInt::new([12345]);
+        let b = BigUInt::new([6789]);
         let result = a * b;
         assert_eq!(result.limbs, [83810205]);
     }
 
     #[test]
     fn mul_square() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, 0, 0, 0] };
-        let b = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, 0, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0, 0, 0]);
+        let b = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0, 0, 0]);
         let result = a * b;
         assert_eq!(result.limbs, [1, 0, 0, u32::MAX - 1, u32::MAX, u32::MAX]);
     }
@@ -266,8 +417,8 @@ mod tests {
 
     #[test]
     fn mul_triple() {
-        let a = BigUInt { limbs: [u32::MAX / 3, u32::MAX / 3, u32::MAX / 3] };
-        let b = BigUInt { limbs: [3, 0, 0] };
+        let a = BigUInt::new([u32::MAX / 3, u32::MAX / 3, u32::MAX / 3]);
+        let b = BigUInt::new([3, 0, 0]);
         let result = a * b;
         assert_eq!(result.limbs, [u32::MAX, u32::MAX, u32::MAX]);
     }
@@ -275,22 +426,22 @@ mod tests {
     #[test]
     #[should_panic]
     fn mul_panic_on_overflow() {
-        let a = BigUInt { limbs: [0, 0, 1, 0] };
-        let b = BigUInt { limbs: [0, 0, 1, 0] };
+        let a = BigUInt::new([0, 0, 1, 0]);
+        let b = BigUInt::new([0, 0, 1, 0]);
         let _ = a * b;
     }
 
     #[test]
     fn mul_max_allowable_mul() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX, 0] };
-        let b = BigUInt { limbs: [0, 1, 0, 0] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0]);
+        let b = BigUInt::new([0, 1, 0, 0]);
         let result = a * b;
         assert_eq!(result.limbs, [0, u32::MAX, u32::MAX, u32::MAX]);
     }
 
     #[test]
     fn div_u32_by_1() {
-        let a = BigUInt { limbs: [12345, 67890, 54321] };
+        let a = BigUInt::new([12345, 67890, 54321]);
         let (quotient, remainder) = a.div_u32(1);
         assert_eq!(quotient.limbs, [12345, 67890, 54321]);
         assert_eq!(remainder, 0);
@@ -306,7 +457,7 @@ mod tests {
 
     #[test]
     fn div_u32_by_smaller_number() {
-        let a = BigUInt { limbs: [123456789, 987654321] };
+        let a = BigUInt::new([123456789, 987654321]);
         let (quotient, remainder) = a.div_u32(12345);
         assert_eq!(quotient.to_str_radix(10), "343616282589837");
         assert_eq!(remainder, 5040);
@@ -314,7 +465,7 @@ mod tests {
 
     #[test]
     fn div_u32_by_power_of_2() {
-        let a = BigUInt { limbs: [123456789, 987654321] };
+        let a = BigUInt::new([123456789, 987654321]);
         let (quotient, remainder) = a.div_u32(8);
         assert_eq!(quotient.to_str_radix(10), "530242876071442850");
         assert_eq!(remainder, 5);
@@ -322,7 +473,7 @@ mod tests {
 
     #[test]
     fn div_u32_by_u32_max() {
-        let a = BigUInt { limbs: [u32::MAX, u32::MAX, u32::MAX] };
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX]);
         let (quotient, remainder) = a.div_u32(u32::MAX);
         assert_eq!(quotient.to_str_radix(10), "18446744078004518913");
         assert_eq!(remainder, 0);
@@ -330,7 +481,7 @@ mod tests {
 
     #[test]
     fn div_u32_large_number() {
-        let a = BigUInt { limbs: [0, 0, u32::MAX] };
+        let a = BigUInt::new([0, 0, u32::MAX]);
         let (quotient, remainder) = a.div_u32(2);
         assert_eq!(quotient.to_str_radix(10), "39614081247908796759917199360");
         assert_eq!(remainder, 0);
@@ -338,7 +489,7 @@ mod tests {
 
     #[test]
     fn div_u32_with_remainder() {
-        let a = BigUInt { limbs: [1, 2, 3] };
+        let a = BigUInt::new([1, 2, 3]);
         let (quotient, remainder) = a.div_u32(2);
         assert_eq!(quotient.limbs, [0, 2147483649, 1]);
         assert_eq!(remainder, 1);
@@ -346,7 +497,7 @@ mod tests {
 
     #[test]
     fn div_u32_complex() {
-        let a = BigUInt { limbs: [1, 1, 1, 1] };
+        let a = BigUInt::new([1, 1, 1, 1]);
         let (quotient, remainder) = a.div_u32(2);
         assert_eq!(quotient.limbs, [2147483648, 2147483648, 2147483648, 0]);
         assert_eq!(remainder, 1);
@@ -373,34 +524,48 @@ mod tests {
     #[test]
     #[should_panic]
     fn sub_overflow() {
-        let a = BigUInt { limbs: [0, u32::MAX, u32::MAX] };
-        let b = BigUInt { limbs: [1, u32::MAX, u32::MAX] };
+        let a = BigUInt::new([0, u32::MAX, u32::MAX]);
+        let b = BigUInt::new([1, u32::MAX, u32::MAX]);
         let result = a - b;
         assert_eq!(result, BigUInt::default());
     }
 
     #[test]
     fn sub_to_zero() {
-        let a = BigUInt { limbs: [1, u32::MAX, u32::MAX] };
-        let b = BigUInt { limbs: [1, u32::MAX, u32::MAX] };
+        let a = BigUInt::new([1, u32::MAX, u32::MAX]);
+        let b = BigUInt::new([1, u32::MAX, u32::MAX]);
         let result = a - b;
         assert_eq!(result, BigUInt::default());
     }
 
     #[test]
     fn sub_with_borrow() {
-        let a = BigUInt { limbs: [0, 0, 1] };
-        let b = BigUInt { limbs: [1, 0, 0] };
+        let a = BigUInt::new([0, 0, 1]);
+        let b = BigUInt::new([1, 0, 0]);
         let result = a - b;
         assert_eq!(result, BigUInt { limbs: [u32::MAX, u32::MAX, 0] });
     }
 
     #[test]
     fn sub_with_borrow_to_zero() {
-        let a = BigUInt { limbs: [0, 0, 1] };
-        let b = BigUInt { limbs: [1, 1, 0] };
+        let a = BigUInt::new([0, 0, 1]);
+        let b = BigUInt::new([1, 1, 0]);
         let result = a - b;
         assert_eq!(result, BigUInt { limbs: [u32::MAX, u32::MAX - 1, 0] });
+    }
+
+
+    #[test]
+    fn div() {
+        let a = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, u32::MAX, 0, 0]);
+        let b = BigUInt::new([u32::MAX, u32::MAX, u32::MAX, 0, 0, 0]);
+        let (q, r) = a.div_rem(b);
+        let c = q * b;
+        let d = c + r;
+        let e = a - d;
+        let j = a + b;
+        let f = "s";
+        // assert_eq!(result.0 * b + result.1, a);
     }
 
 }
